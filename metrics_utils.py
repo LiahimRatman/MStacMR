45eec8059a01a3eb0082b052423d0d56cdca18ee
Mikhail Korotkov <ma-korotkov@yandex.ru>
2021-12-04 11:17:31 +0300

    MStacMR | metrics_utils + search_service | add metrics_utils + search service (all i2t + t2i functions)

---
+++ metrics_utils.py
@@ -0,0 +1,38 @@
+import logging
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+
+    return res
+
+
+def validate(opt, val_loader, model):
+    # compute the encoding for all the validation images and captions
+    img_embs, cap_embs = encode_data(
+        model, val_loader, opt.log_step, logging.info)
+
+    # caption retrieval
+    (r1, r5, r10, medr, meanr) = i2t(img_embs, cap_embs, measure=opt.measure)
+    print("Image to text: %.1f, %.1f, %.1f, %.1f, %.1f" %
+                 (r1, r5, r10, medr, meanr))
+    # image retrieval
+    (r1i, r5i, r10i, medri, meanr) = t2i(
+        img_embs, cap_embs, measure=opt.measure)
+    print("Text to image: %.1f, %.1f, %.1f, %.1f, %.1f" %
+                 (r1i, r5i, r10i, medri, meanr))
+    # sum of recalls to be used for early stopping
+    currscore = r1 + r5 + r1i + r5i
+
+    return currscore
---
+++ search_service.py
@@ -0,0 +1,27 @@
+import torch
+
+
+def cosine_sim(im, s):
+    """Cosine similarity between all the image and sentence pairs
+    """
+
+    return im.mm(s.t())
+
+
+def order_sim(im, s):
+    """Order embeddings similarity measure $max(0, s-im)$
+    """
+    YmX = (s.unsqueeze(1).expand(s.size(0), im.size(0), s.size(1))
+           - im.unsqueeze(0).expand(s.size(0), im.size(0), s.size(1)))
+    score = -YmX.clamp(min=0).pow(2).sum(2).sqrt().t()
+
+    return score
+
+
+def l2norm(X):
+    """L2-normalize columns of X
+    """
+    norm = torch.pow(X, 2).sum(dim=1, keepdim=True).sqrt()
+    X = torch.div(X, norm)
+
+    return X
