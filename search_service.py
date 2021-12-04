260a8fce76aa69b763769a49c68f549f7e776d3a
Mikhail Korotkov <ma-korotkov@yandex.ru>
2021-12-04 20:50:46 +0300

    MStacMR | search_service | add t2i and i2t functions

--- search_service.py
+++ search_service.py
@@ -1,4 +1,6 @@
+import numpy as np
 import torch
+from tqdm import tqdm
 
 
 def cosine_sim(im, s):
@@ -25,3 +27,139 @@ def l2norm(X):
     X = torch.div(X, norm)
 
     return X
+
+
+def i2t(images, captions, npts=None, measure='cosine', return_ranks=False):
+    """
+    Images->Text (Image Annotation)
+    Images: (5N, K) matrix of images
+    Captions: (5N, K) matrix of captions
+    """
+    if npts is None:
+        npts = int(images.shape[0] / 5)
+    index_list = []
+
+    ranks = np.zeros(npts)
+    top1 = np.zeros(npts)
+    for index in range(npts):
+
+        # Get query image
+        im = images[5 * index].reshape(1, images.shape[1])
+
+        # Compute scores
+        if measure == 'order':
+            bs = 100
+            if index % bs == 0:
+                mx = min(images.shape[0], 5 * (index + bs))
+                im2 = images[5 * index:mx:5]
+                d2 = order_sim(torch.Tensor(im2),#.cuda(),
+                               torch.Tensor(captions))#.cuda())
+                d2 = d2.cpu().numpy()
+            d = d2[index % bs]
+        else:
+            d = np.dot(im, captions.T).flatten()
+        inds = np.argsort(d)[::-1]
+        index_list.append(inds[0])
+
+        # Score
+        rank = 1e20
+        for i in range(5 * index, 5 * index + 5, 1):
+            tmp = np.where(inds == i)[0][0]
+            if tmp < rank:
+                rank = tmp
+        ranks[index] = rank
+        top1[index] = inds[0]
+
+    # Compute metrics
+    r1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    r5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    r10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    medr = np.floor(np.median(ranks)) + 1
+    meanr = ranks.mean() + 1
+    if return_ranks:
+        return (r1, r5, r10, medr, meanr), (ranks, top1)
+    else:
+        return (r1, r5, r10, medr, meanr)
+
+
+def t2i(images, captions, npts=None, measure='cosine', return_ranks=False):
+    """
+    Text->Images (Image Search)
+    Images: (5N, K) matrix of images
+    Captions: (5N, K) matrix of captions
+    """
+    if npts is None:
+        npts = int(images.shape[0] / 5)
+    ims = np.array([images[i] for i in range(0, len(images), 5)])
+
+    ranks = np.zeros(5 * npts)
+    top1 = np.zeros(5 * npts)
+    for index in range(npts):
+
+        # Get query captions
+        queries = captions[5 * index:5 * index + 5]
+
+        # Compute scores
+        if measure == 'order':
+            bs = 100
+            if 5 * index % bs == 0:
+                mx = min(captions.shape[0], 5 * index + bs)
+                q2 = captions[5 * index:mx]
+                d2 = order_sim(torch.Tensor(ims),#.cuda(),
+                               torch.Tensor(q2))#.cuda())
+                d2 = d2.cpu().numpy()
+
+            d = d2[:, (5 * index) % bs:(5 * index) % bs + 5].T
+        else:
+            d = np.dot(queries, ims.T)
+        inds = np.zeros(d.shape)
+        for i in range(len(inds)):
+            inds[i] = np.argsort(d[i])[::-1]
+            ranks[5 * index + i] = np.where(inds[i] == index)[0][0]
+            top1[5 * index + i] = inds[i][0]
+
+    # Compute metrics
+    r1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    r5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    r10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+    medr = np.floor(np.median(ranks)) + 1
+    meanr = ranks.mean() + 1
+    if return_ranks:
+        return (r1, r5, r10, medr, meanr), (ranks, top1)
+    else:
+        return (r1, r5, r10, medr, meanr)
+
+
+def encode_data(model, data_loader, log_step=10, logging=print):
+    """Encode all images and captions loadable by `data_loader`
+    """
+    # batch_time = AverageMeter()
+    # val_logger = LogCollector()
+
+    # switch to evaluate mode
+    model.val_start()
+
+    # end = time.time()
+
+    # numpy array to keep all the embeddings
+    img_embs = None
+    cap_embs = None
+    # image (Tensor), caption (Encoded by NLTK), caption length, image id, caption (Encoded and with padding up to max_len), caption mask (the same but mask), scene text (Encoded, 15 texts per image with emb len 300)
+    for (images, captions, lengths, ids, caption_labels, caption_masks, scene_text) in tqdm(data_loader):
+        # make sure val logger is used
+        # model.logger = val_logger
+
+        # compute the embeddings
+        img_emb, cap_emb, GCN_img_emd = model.forward_emb(images, captions, lengths, scene_text, volatile=True)
+
+        # initialize the numpy arrays given the size of the embeddings
+        if img_embs is None:
+            img_embs = np.zeros((len(data_loader.dataset), img_emb.size(1)))
+            cap_embs = np.zeros((len(data_loader.dataset), cap_emb.size(1)))
+
+        img_embs[ids, :] = img_emb.data.cpu().numpy().copy()
+        cap_embs[ids, :] = cap_emb.data.cpu().numpy().copy()
+
+        # del images, captions
+
+    return img_embs, cap_embs
