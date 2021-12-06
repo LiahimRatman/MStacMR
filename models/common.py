793b5899d29f905210225c27255522a3716badf3
Mikhail Korotkov <ma-korotkov@yandex.ru>
2021-12-06 16:56:57 +0300

    MStacMR | yolo lib | fix yolo dependencies

--- models/common.py
+++ models/common.py
@@ -287,7 +287,7 @@ class DetectMultiBackend(nn.Module):
         #   ONNX Runtime:           *.onnx
         #   OpenCV DNN:             *.onnx with dnn=True
         #   TensorRT:               *.engine
-        from caption_models import attempt_download, attempt_load  # scoped to avoid circular import
+        from models.experimental import attempt_download, attempt_load  # scoped to avoid circular import
 
         super().__init__()
         w = str(weights[0] if isinstance(weights, list) else weights)
